62757370548da76c20b59e9b530ca099124a418e
loris.lacroix <loris.lacroix@iut-dijon.u-bourgogne.fr>
2024-11-19 15:53:03 +0100

    affichage des personnages joli

--- Views/home.php
+++ Views/home.php
@@ -1,5 +1,27 @@
-<?php var_dump($listUnit); ?>
-
-<?php var_dump($first); ?>
-
-<?php var_dump($other); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Units</title>
+    <link rel="stylesheet" href="/public/css/main.css">
+</head>
+<body>
+<div class="container">
+    <?php foreach ($listUnit as $unit): ?>
+        <div class="unit-card">
+            <img src="<?php echo htmlspecialchars($unit->getUrlImg()); ?>" alt="<?php echo htmlspecialchars($unit->getName()); ?>" class="unit-image">
+            <div class="unit-details">
+                <h2><?php echo htmlspecialchars($unit->getName()); ?></h2>
+                <p>Cost: <?php echo htmlspecialchars($unit->getCost()); ?></p>
+                <p>Origin: <?php echo htmlspecialchars($unit->getOrigin()); ?></p>
+                <div class="options">
+                    <a href="edit.php?id=<?php echo htmlspecialchars($unit->getId()); ?>" class="btn btn-edit">Edit</a>
+                    <a href="delete.php?id=<?php echo htmlspecialchars($unit->getId()); ?>" class="btn btn-delete">Delete</a>
+                </div>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</div>
+</body>
+</html>
