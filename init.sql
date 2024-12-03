bcc434c67f73ced9cadf212b7f563adab350f2e6
loris.lacroix <loris.lacroix@iut-dijon.u-bourgogne.fr>
2024-12-03 23:43:13 +0100

    pour la base de données

---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE UNIT (
+                      id VARCHAR(255) PRIMARY KEY,
+                      name VARCHAR(255) NOT NULL,
+                      cost INT NOT NULL,
+                      origin VARCHAR(255) NOT NULL,
+                      url_img VARCHAR(15000) NOT NULL
+);
